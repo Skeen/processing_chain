d4fc91e9226232bfbd697b40788ce3cc181b08f5
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-01-06 19:48:02 +0100

    Check MD5s for tar files!

--- tools/dtw.mk
+++ tools/dtw.mk
@@ -6,9 +6,24 @@ $(KNN_DTW_NAME): $(JOBFILE_QRY) $(JOBFILE_REF)
 	@mkdir -p $(dir $@)
 	$(REMOTE_KNN_DTW_REQUEST) $(JOBFILE_REF) $(JOBFILE_QRY) "" $(REMOTE_KNN_SPLIT) $(REMOTE_KNN_TIMEOUT) > $@
 
-$(KNN_DTW_TAR): $(KNN_DTW_NAME)
+$(KNN_DTW_TAR).download: $(KNN_DTW_NAME)
 	@mkdir -p $(dir $@)
 	cat $< | xargs $(REMOTE_KNN_DTW_RESPONSE) > $@
+
+$(KNN_DTW_TAR).md5: $(KNN_DTW_TAR).download
+	@mkdir -p $(dir $@)
+	md5sum $< | cut -f1 -d' ' | tr --delete '\n' > $@
+
+$(KNN_DTW_TAR): $(KNN_DTW_TAR).download $(KNN_DTW_TAR).md5 $(KNN_DTW_NAME)
+	@mkdir -p $(dir $@)
+	@if [ "$(shell cat $(KNN_DTW_NAME) | xargs $(REMOTE_KNN_DTW_MD5))" = "$(shell cat $(KNN_DTW_TAR).md5)" ]; then \
+		echo "MD5 check passed: '$@'"; \
+		cp $(KNN_DTW_TAR).download $@; \
+	else \
+		echo "MD5 check failed: '$@', removing $<"; \
+		rm $(KNN_DTW_TAR).download $(KNN_DTW_TAR).md5; \
+		false; \
+	fi
 else
 # .jobfiles to .json
 $(KNN_DTW_TAR).source: $(JOBFILE_QRY) $(JOBFILE_REF)
@@ -25,9 +40,24 @@ $(KNN_DTW_MODEL_NAME): $(JOBFILE_COMBINED)
 	@mkdir -p $(dir $@)
 	$(REMOTE_KNN_DTW_REQUEST) $(JOBFILE_COMBINED) $(JOBFILE_COMBINED) "-m" $(REMOTE_KNN_SPLIT) $(REMOTE_KNN_TIMEOUT) > $@
 
-$(KNN_DTW_MODEL_TAR): $(KNN_DTW_MODEL_NAME)
+$(KNN_DTW_MODEL_TAR).download: $(KNN_DTW_MODEL_NAME)
 	@mkdir -p $(dir $@)
 	cat $< | xargs $(REMOTE_KNN_DTW_RESPONSE) > $@
+
+$(KNN_DTW_MODEL_TAR).md5: $(KNN_DTW_MODEL_TAR).download
+	@mkdir -p $(dir $@)
+	md5sum $< | cut -f1 -d' ' | tr --delete '\n' > $@
+
+$(KNN_DTW_MODEL_TAR): $(KNN_DTW_MODEL_TAR).download $(KNN_DTW_MODEL_TAR).md5 $(KNN_DTW_MODEL_NAME)
+	@mkdir -p $(dir $@)
+	@if [ "$(shell cat $(KNN_DTW_MODEL_NAME) | xargs $(REMOTE_KNN_DTW_MD5))" = "$(shell cat $(KNN_DTW_MODEL_TAR).md5)" ]; then \
+		echo "MD5 check passed: '$@'"; \
+		cp $(KNN_DTW_MODEL_TAR).download $@; \
+	else \
+		echo "MD5 check failed: '$@', removing $<"; \
+		rm $(KNN_DTW_MODEL_TAR).download $(KNN_DTW_MODEL_TAR).md5; \
+		false; \
+	fi
 else
 # .jobfiles to .json
 $(KNN_DTW_MODEL_TAR).source: $(JOBFILE_COMBINED)
